b7b5269d2e50a31174fc7a89d80aff8ab3042f8f
Emon02 <abdulalimemon11@gmail.com>
2023-03-11 23:40:25 +0600

    Mobile menu

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import { CgMenuGridO } from 'react-icons/cg';
 import { MdOutlineClose } from "react-icons/md";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import emon from '../assets/emon.png';
+
+import { MdContactMail, MdMarkEmailUnread } from 'react-icons/md';
+import { BsEnvelopeCheckFill } from 'react-icons/bs';
+import { IoHome } from 'react-icons/io5';
 
 const MobileMenu = () => {
     const [showSidebar, setShowSidebar] = useState(false);
@@ -19,32 +24,47 @@ const MobileMenu = () => {
             <div className={`top-0 right-0 w-10/12  px-5 py-8 bg-[#161B22] fixed h-screen min-h-screen z-50 overflow-y-auto ease-in-out duration-500 ${showSidebar ? "translate-x-0 " : "translate-x-full"
                 }`} >
 
-
-                <div className='flex justify-between items-center'>
-                    <h2 className="text-2xl text-white fixed z-50 flex items-center justify-center">
-                        <HiOutlineMenuAlt1 className='w-7 h-7 error-text border-0 mr-4'></HiOutlineMenuAlt1>
-                        Menu
-                    </h2>
+                <div className='flex justify-between items-center py-5'>
+                    <img src={emon} alt='Emon' className="w-36 h-14 fixed z-50 flex items-center justify-center"></img>
                     <button
-                        className="text-xl error-text cursor-pointer fixed right-7 z-50"
+                        className="text-xl bg-secondary rounded-full p-2 cursor-pointer fixed right-7 z-50"
                         onClick={() => setShowSidebar(!showSidebar)}
                     >
-                        <MdOutlineClose className='w-7 h-7 error-text border-0'></MdOutlineClose>
+                        <MdOutlineClose className='w-6 h-6 text-black border-0'></MdOutlineClose>
                     </button>
                 </div>
 
 
-                <div >
-                    <div className="flex items-center p-2 space-x-4 success h-20 mt-8">
-                        <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="w-12 h-12 rounded-full" />
-                        <div>
-                            <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
-                            <span className="flex items-center space-x-1">
-                                <Link className="text-xs hover:underline ">View profile</Link>
-                            </span>
+                <div className="p-5 mt-10">
+                    <div >
+                        <div className="space-y-8 flex items-center justify-center flex-col">
+                            <NavLink to='/' className={({ isActive }) =>
+                                isActive
+                                    ? "text-secondary font-bold mr-2 flex items-center text-xl"
+                                    : "text-neutral mr-2 flex items-center text-xl"}>
+                                <IoHome className='w-8 h-8 mr-5 text-secondary'></IoHome>
+                                Home
+                            </NavLink>
+
+                            <NavLink to='/contact' className={({ isActive }) =>
+                                isActive
+                                    ? "text-secondary font-bold mr-2 flex items-center text-xl"
+                                    : "text-neutral mr-2 flex items-center text-xl"}>
+                                <MdContactMail className='w-8 h-8 mr-5 text-secondary'></MdContactMail>
+                                Contact
+                            </NavLink>
+
+                            <NavLink to='/home' className={({ isActive }) =>
+                                isActive
+                                    ? "text-secondary font-bold mr-2 flex items-center text-xl"
+                                    : "text-neutral mr-2 flex items-center text-xl"}>
+                                <IoHome className='w-8 h-8 mr-5 text-secondary'></IoHome>
+                                About
+                            </NavLink>
+                            
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
